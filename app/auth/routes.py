69c55248c1c6efd67a2a15bc210e5268b3fa126a
EC2 Default User <ec2-user@ip-172-31-49-148.ec2.internal>
2021-05-01 08:38:13 +0000

    updated routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,17 @@
-from flask import redirect, url_for, flash, request, render_template, abort, Response
+from flask import Flask, redirect, url_for, flash, request, render_template, abort, Response
 from flask_babel import _
 from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 from werkzeug.utils import secure_filename
-
+import os
+import urllib.request
 from app import db
 from app.auth import bp
 from app.auth.email import send_password_reset_email
 from app.auth.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Img
+from datetime import datetime
+
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -41,7 +44,7 @@ def register():
         return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data,phonenumber=form.phonenumber.data)
+        user = User(username=form.username.data, email=form.email.data, phonenumber=form.phonenumber.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
@@ -101,9 +104,45 @@ def upload():
     return render_template('index.html', title=_('Image'))
 
 
-@bp.route('/<int:id>')
-def get_img(id):
-    img = Img.query.filter_by(id=id).first()
-    if not img:
-        return 'No img with that id'
-    return render_template('index.html', title=_('Image'))
+bp = Flask(__name__)
+
+bp.secret_key = "caircocoders-ednalan"
+
+bp.config['MYSQL_HOST'] = 'localhost'
+bp.config['MYSQL_USER'] = 'root'
+bp.config['MYSQL_PASSWORD'] = ''
+bp.config['MYSQL_DB'] = 'testingdb'
+bp.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+
+UPLOAD_FOLDER = 'static/uploads'
+bp.config['UPLOAD_FOLADER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplist('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route("/upload",methods=["POST","GET"])
+def upload():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    now = datetime.now()
+    if request.method == 'POST':
+        files = request.files.getlist('files[]')
+        print(files)
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                cur.execute("INSERT INTO images (file_name, uploaded_on) VALUES (%s, %s)",[filename, now])
+                mysql.connection.commit()
+            print(file)
+        cur.close()
+        flash('File(s) successfully uploaded')
+    return redirect('/')
